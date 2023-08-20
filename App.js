2f1a1356f2aa5fded90d3ce02025f2fd91048b6f
Virendra-Mishra1920 <mishravirendra1920@gmail.com>
2023-08-20 22:30:32 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/* 
+    <div id="parent">
+        <div id="child">
+            <h1>I am tag h1</h1>
+            <h2>I am tag h2</h2>
+        </div>
+
+        <div id="child2">
+            <h1>I am tag h1</h1>
+             <h2>I am tag h2</h2>
+        </div>
+    </div>
+
+
+*/
+
+// JSX: we use JSX to create complext html syntax but not createHtml. 
+// CreateHtml is a core syntax of react itslef.
+
+var parent= React.createElement("div", {id:"parent"}, 
+    [React.createElement("div", {id:"child"}, 
+    [React.createElement("h1",{},"I am h1 tag"), React.createElement("h2",{},"I am h2 tag")]),
+
+    React.createElement("div", {id:"child2"}, 
+    [React.createElement("h1",{},"I am h1 tag"), React.createElement("h2",{},"I am h2 tag")]),
+
+    React.createElement("div", {id:"child3"}, 
+    [React.createElement("h1",{},"I am h1 tag"), React.createElement("h2",{},"I am h2 tag")]),
+    ])
+ 
+//  var heading=React.createElement(
+//     "h1",
+//     {id:"heading",xyz:"abc"},
+//     "Hello, World from React"
+//     );
+
+    console.log(parent) // object
+ var root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent); //creating html element and putting into DOM
