e8a77038b12906811ee2d6fc4a6502fe5330bdf4
Virendra-Mishra1920 <mishravirendra1920@gmail.com>
2023-08-23 09:06:19 +0530

    Implemented parcel bundler in my app

--- App.js
+++ App.js
@@ -17,6 +17,9 @@
 // JSX: we use JSX to create complext html syntax but not createHtml. 
 // CreateHtml is a core syntax of react itslef.
 
+import React from "react"
+import ReactDOM from 'react-dom'
+
 var parent= React.createElement("div", {id:"parent"}, 
     [React.createElement("div", {id:"child"}, 
     [React.createElement("h1",{},"I am h1 tag"), React.createElement("h2",{},"I am h2 tag")]),
---
+++ README.md
@@ -0,0 +1,14 @@
+Parcel
+#dev server
+#build
+#HMR- hot module replacer
+#File Watchin Algorithm
+#Caching - build faster
+#Minification
+#Compress
+#Bundling
+#Tree Shaking - remove unused code from project
+#Differential Bundling
+#Diganostic
+#Error Handling
+#Different build for dev & prod
